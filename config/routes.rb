b54b5101abc12426b451363f16792c61a3d04e3c
danburudoa <yumamukai515@gmail.com>
2022-09-09 16:54:07 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'home#top'
   devise_for :users
   resource :user, except: [:new, :create, :destroy]
-  resources :recipes
+  resources :recipes do
+    resource :favorites, only: [:create, :destroy]
+  end
+
 end
