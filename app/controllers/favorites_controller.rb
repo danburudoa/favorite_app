1569ddd198d62424056dfe941f5cd3dd18b65bff
danburudoa <yumamukai515@gmail.com>
2022-09-10 15:04:42 +0900

    いいねのcreateとdeleteメソッド記述変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,14 @@ class FavoritesController < ApplicationController
   def create
     # @favorite = Favorite.new(user_id: current_user.id, recipe_id: params[:recipe_id])
     # 上記は、最初に書いた文で間違っていないが、下記の文の方が良い。
-    @favorite = current_user.favorites.create(recipe_id: params[:recipe_id])
-    redirect_back(fallback_location: root_path)
+    @recipe = Recipe.find(params[:recipe_id])
+    favorite = current_user.favorites.new(recipe_id: @recipe.id)
+    favorite.save
   end
 
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
-    @favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
-    @favorite.destroy
-    redirect_back(fallback_location: root_path)
+    favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
+    favorite.destroy
   end
 end
